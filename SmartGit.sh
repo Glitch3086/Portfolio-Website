4922b0503be8450ed61d4c52f6395774ea21e416
Glitch3086 <Glitch3086@gmail.com>
2022-05-10 17:00:17 -0500

    Smart git added

---
+++ SmartGit.sh
@@ -0,0 +1,8 @@
+cd Portfolio Eebsite/
+git add --all
+echo "------------In Progress ----------"
+git commit -m"Repo Updated"
+echo "Git commited successfully"
+echo "Pushing Repo"
+echo "Provide Credentials for Gitthub Glitch3086"
+git push
